8c39ac14fa69cdbf0d715447d2d2ba4b71a21493
Ludvig Jönsson <ludvigjonsson28@gmail.com>
2021-01-03 01:53:57 +0100

    updated testClass

--- PlantoBotWorkSpace/src/main/java/models/TestClass.java
+++ PlantoBotWorkSpace/src/main/java/models/TestClass.java
@@ -3,4 +3,12 @@ package main.java.models;
 public class TestClass {
 
     private String testString;
+
+    public String getTestString() {
+        return testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }
 }
